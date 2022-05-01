9ab8e0d3d508b69b795d03cd2875fc0385ea416b
Alvin Setiawan <49272368+sabo99@users.noreply.github.com>
2022-05-01 21:43:27 +0700

    update index

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -11,7 +11,7 @@
     <!-- mobile viwport meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 
     {{-- Bootstrap 5.0.2 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
