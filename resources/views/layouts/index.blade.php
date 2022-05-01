7fc231fae50f2f8727647b6836508855fa5292af
Alvin Setiawan <49272368+sabo99@users.noreply.github.com>
2022-05-01 21:40:52 +0700

    add meta data

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,8 +1,18 @@
 <html>
 
 <head>
-    <title>Image Optimize</title>
+    <title>Image Optimizer</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- meta description -->
+    <meta name="description" content="">
+    <!-- mobile viwport meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
     {{-- Bootstrap 5.0.2 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
